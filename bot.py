9eb02e3935a2e4e0d71012da96dc4e545493deae
zigelzi <miika.a.savela@gmail.com>
2021-08-21 19:53:55 +0300

    Add Dockerfiled and test for communicating between containers

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -8,7 +9,7 @@ from utilities import is_valid_url
 load_dotenv('envs/.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-print(TOKEN)
+
 bot = commands.Bot(command_prefix='/')
 
 @bot.command(name='add')
@@ -32,6 +33,19 @@ async def add_movie(ctx, url, service):
         movie_request['name'] = get_movie_name(url)
         # add_movie_to_sheet(movie_request)
         await ctx.channel.send(f'Requested movie: {url} in service {service}')
+
+@bot.command(name='ping')
+async def ping(ctx):
+    if ctx.channel.name == 'bottihärö':
+        movie_request_json = {
+            "message_id":"111aaa",
+            "name":"Post Movie",
+            "service": "Netflix",
+            "suggested_by":"Dvh",
+            "url":"www.google.com"
+        }
+        r = requests.post('http://localhost:5000/movie_suggestions', data=movie_request_json)
+        await ctx.channel.send(r.text)
 
 @add_movie.error
 async def add_error(ctx, error):
